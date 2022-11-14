c235c2701753340b74f49859b95eceedc6f1e6d8
Maxim <maks.bidenko.96@mail.ru>
2022-11-14 12:09:59 +0300

    Сделал 1 дескриптор

--- ex01.py
+++ ex01.py
@@ -1,17 +1,30 @@
+class NonNegative:
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.my_attr] 
+
+    def __set__(self, instance, value):
+        if value < 0:
+            raise ValueError('Введеные данные не могут быть отрицательными! ')
+        instance.__dict__[self.my_attr] = value 
+
+    def __set_name__(self, owner, my_attr):
+        self.my_attr = my_attr
+
+
 class Road:
-    __length = None
-    __width = None
+    length = NonNegative()
+    width = NonNegative()
     weigth = None
     tickness = None
 
     def __init__(self, length, width):
-        self.__length = length
-        self.__width = width
+        self.length = length
+        self.width = width
 
     def intake(self):
         self.weigth = 25
         self.tickness = 0.05
-        intake = self.__length * self.__width * self.weigth * self.tickness / 1000
+        intake = self.length * self.width * self.weigth * self.tickness / 1000
         print(f'Требуется {intake} тонн для покрытия 1 кв. метра')
 
 
