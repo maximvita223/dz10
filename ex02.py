6467831ed0978486b8ed73e2d3e8e495903f7ad3
Maxim <maks.bidenko.96@mail.ru>
2022-11-14 12:11:06 +0300

    Сделал 2 дескриптор

--- ex02.py
+++ ex02.py
@@ -1,10 +1,22 @@
+class NonNegative:
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.my_attr]
+
+    def __set__(self, instance, value):
+        if value < 0:
+            raise ValueError('Введеные данные не могут быть отрицательными! ')
+        instance.__dict__[self.my_attr] = value
+
+    def __set_name__(self, owner, my_attr):
+        self.my_attr = my_attr
+
 class Worker:
 
     name = None
     surname = None
     position = None
-    profit = None
-    bonus = None
+    profit = NonNegative()
+    bonus = NonNegative()
 
     def __init__(self, name, surname, position, profit, bonus):
         self.name = name
